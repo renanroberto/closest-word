eca3f8e07ef0722419c2eef79d888e3552c0f53e
Renan <renanroberto1@gmail.com>
2021-08-14 14:49:21 -0300

    Create hamming distance

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
-{-@ LIQUID "--compilespec" @-}
 module Main where
+
+{-@ hamming :: s:String -> {r:String | len s = len r} -> Nat @-}
+hamming :: String -> String -> Int
+hamming xs ys = go 0 xs ys
+  where go :: Int -> String -> String -> Int
+        go acc [] [] = acc
+        go acc (x:xs) (y:ys)
+          | x == y = go acc xs ys
+          | otherwise = go (acc + 1) xs ys
 
 main :: IO ()
 main = putStrLn "Hello"
